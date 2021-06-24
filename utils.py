c3f16476167d01f3a5718254a1940b4675ee034a
len <l.yoshida.132@gmail.com>
2021-06-25 03:58:53 +0900

    implement sock_Sign

--- utils.py
+++ utils.py
@@ -36,10 +36,16 @@ class Socket_separate:
 
 
 class Socket_Sign:
-    # sturct.pack('!i', 100)などを使ってint2bytesする？
-    def __init__(self, sock=None):
+    def __init__(self, sock=None, digit=4):
+        '''
+        digit: digit of len(msg)
+
+        digit = 4 means len(msg) < 4 bytes (uint)
+        '''
+
         self.sock = sock or socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.buff = b''
+        self.digit = digit
 
     def connect(self, target):
         self.sock.connect(target)
@@ -48,18 +54,38 @@ class Socket_Sign:
         self.sock.close()
 
     def send(self, msg:str):
+        msg = msg.encode()
         msg_len = len(msg)
-        self.__send()
-        pass
+        self.__send(self.int2bytes(msg_len))
+        self.__send(msg)
 
     def __send(self, msg:bytes):
-        # msg need to be bytes object
         totalsent = 0
         while totalsent < len(msg):
-            sent = self.sock.send(msg[totalsent:].encode())
+            sent = self.sock.send(msg[totalsent:])
             if sent == 0:
                 raise RuntimeError('[!] socket connection broken')
             totalsent += sent
 
-    def recv(self, buff_size:int):
-        pass
+    def recv(self, buff_size:int)->str:
+        msg_len = self.bytes2int(self.__recv(self.digit))
+        msg = self.__recv(msg_len).decode()
+        return msg
+
+    def __recv(self, buff_size:int)->bytes:
+        while 1:
+            if len(self.buff) >= buff_size:
+                break
+            data = self.sock.recv(buff_size)
+            if data == b'':
+                raise RuntimeError('[!] socket connection broken')
+            self.buff += data
+        msg = self.buff[:buff_size]
+        self.buff = self.buff[buff_size:]
+        return msg
+
+    def int2bytes(self, num:int):
+        return num.to_bytes(self.digit, 'big')
+
+    def bytes2int(self, num:bytes):
+        return int.from_bytes(num, 'big')
