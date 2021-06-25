d5e63ee43210f18a16d64c62b036ed24088f6fbb
len <l.yoshida.132@gmail.com>
2021-06-25 17:55:10 +0900

    add argparser

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import argparse
 from utils import *
 
 
@@ -5,10 +6,15 @@ host = socket.gethostname()
 port = 7777
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target', '-t', required=True)
+    parser.add_argument('--port', '-p', type=int, required=True)
+    args = parser.parse_args()
+
     try:
         sock = Socket_Sign()
-        sock.connect((host,port))
-        sock.send('SYN'*10)
+        sock.connect((args.target,args.port))
+        sock.send('SYN')
         msg = sock.recv()
         print(msg)
         sock.send('ACK')
